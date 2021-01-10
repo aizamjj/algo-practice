2fdb740813bc1b7ae9c50a6ad8fce08dc3a7d5f6
AJ <aj@speakfully.com>
2021-01-09 19:35:03 -0800

    Debug freedomTrail

--- freedomTrail.js
+++ freedomTrail.js
@@ -1,30 +1,48 @@
 function mod(x, m) {
     return (x % m + m) % m;
 }
+function fill(n, s) {
+    var array = [];
+    while (n > 0) {
+        array.push(s);
+        n--;
+    }
+    return array;
+}
 function solve(ring, key, keyIndex, ringIndex) {
     if (keyIndex === void 0) { keyIndex = 0; }
     if (ringIndex === void 0) { ringIndex = 0; }
     if (keyIndex === key.length) {
-        return 0;
+        return [];
     }
     var char = key[keyIndex];
     var leftRing = ring.slice(0, ringIndex + 1).split('').reverse().join('') + ring.slice(ringIndex + 1).split('').reverse().join('');
     var rightRing = ring.slice(ringIndex) + ring.slice(0, ringIndex);
     var rightIndex = rightRing.indexOf(char);
     var leftIndex = leftRing.indexOf(char);
-    var stupidLeftRingIndex = mod((ringIndex - leftIndex), ring.length);
-    var stupidRightRingIndex = mod((ringIndex + rightIndex), ring.length);
-    var minDistance = Math.min(solve(ring, key, keyIndex + 1, stupidLeftRingIndex) || leftIndex, solve(ring, key, keyIndex + 1, stupidRightRingIndex) || rightIndex);
-    console.log('solve for:', char, 'right:', rightIndex, 'left:', leftIndex, 'rightRing:', rightRing, 'leftRing:', leftRing, 'ringIndex:', ringIndex, 'leftRingIndex:', stupidLeftRingIndex, 'rightRingIndex:', stupidRightRingIndex, 'mindistance:', minDistance);
-    return minDistance + 1;
+    var lIndex = mod((ringIndex - leftIndex), ring.length);
+    var rIndex = mod((ringIndex + rightIndex), ring.length);
+    var leftPath = fill(lIndex, '<-').concat('.');
+    var rightPath = fill(rIndex, '->').concat('.');
+    leftPath = leftPath.concat(solve(ring, key, keyIndex + 1, leftIndex));
+    rightPath = rightPath.concat(solve(ring, key, keyIndex + 1, rightIndex));
+    console.log('right', rightPath, char, rIndex, rightIndex, rightRing);
+    console.log('left', leftPath, char, lIndex, leftIndex, leftRing);
+    var min = leftPath.length < rightPath.length ? leftPath : rightPath;
+    // let minDistance = Math.min((leftPath.concat(
+    //   solve(ring, key, keyIndex + 1, leftIndex))).length, (rightPath.concat( 
+    //   solve(ring, key, keyIndex + 1, rightIndex)).length
+    // )
+    return min;
 }
 // test
-// const result: number = solve('godding', 'gd', 0, 0);
+// const result = solve('godding', 'gd', 0, 0);
 // console.log(solve('godding', 'godding', 0, 0))
 var result = solve('godding', 'dn', 0, 0);
 console.log(result);
+// dn
 //        'd'
 //      /    \
-//      4     2
+//      4     2 
 //   /   \   /  \
-//  4     2 4    3
+//  4     2 4    3  n
--- freedomTrail.ts
+++ freedomTrail.ts
@@ -1,46 +1,56 @@
 function mod(x, m) {
   return (x%m + m)%m;
 }
-function solve(ring: string, key: string, keyIndex: number=0, ringIndex: number=0): number {
-  if (keyIndex === key.length) {
-    return 0; 
-  }    
-  let char: string = key[keyIndex];
 
+function fill(n, s) {
+  let array = [];
+  while (n > 0) {
+    array.push(s)
+    n--;
+  }
+  return array;
+}
+
+function solve(ring: string, key: string, keyIndex: number=0, ringIndex: number=0): string[] {
+  
+  if (keyIndex === key.length) {
+    return []; 
+  } 
+  
+  let char: string = key[keyIndex];
+  
   const leftRing = ring.slice(0, ringIndex + 1).split('').reverse(). join('') + ring.slice(ringIndex + 1).split('').reverse().join('')
   const rightRing = ring.slice(ringIndex) + ring.slice(0, ringIndex)
     
   let rightIndex: number = rightRing.indexOf(char);
   let leftIndex: number = leftRing.indexOf(char);
-  let stupidLeftRingIndex = mod((ringIndex - leftIndex), ring.length);
-  let stupidRightRingIndex = mod((ringIndex + rightIndex), ring.length);
-  let minDistance: number = Math.min(
-    solve(ring, key, keyIndex + 1, stupidLeftRingIndex) || leftIndex, 
-    solve(ring, key, keyIndex + 1, stupidRightRingIndex) || rightIndex
-  )
-  console.log(
-      'solve for:', char,
-      'right:', rightIndex, 
-      'left:', leftIndex, 
-      'rightRing:', rightRing, 
-      'leftRing:', leftRing, 
-      'ringIndex:', ringIndex, 
-      'leftRingIndex:', stupidLeftRingIndex, 
-      'rightRingIndex:', stupidRightRingIndex,
-      'mindistance:', minDistance
-      )
-  return minDistance + 1;
+  
+  let lIndex = mod((ringIndex - leftIndex), ring.length);  
+  let rIndex = mod((ringIndex + rightIndex), ring.length);
+  let leftPath = fill(lIndex, '<-').concat('.');
+  let rightPath = fill(rIndex, '->').concat('.');
+  leftPath = leftPath.concat(solve(ring, key, keyIndex + 1, leftIndex))
+  rightPath = rightPath.concat( 
+    solve(ring, key, keyIndex + 1, rightIndex))
+    console.log('right', rightPath, char, rIndex, rightIndex, rightRing);
+    console.log('left', leftPath, char, lIndex, leftIndex, leftRing)
+  let min = leftPath.length < rightPath.length ? leftPath : rightPath;
+  // let minDistance = Math.min((leftPath.concat(
+  //   solve(ring, key, keyIndex + 1, leftIndex))).length, (rightPath.concat( 
+  //   solve(ring, key, keyIndex + 1, rightIndex)).length
+  // )
+  return min;
 }
 
 // test
-// const result: number = solve('godding', 'gd', 0, 0);
+// const result = solve('godding', 'gd', 0, 0);
 // console.log(solve('godding', 'godding', 0, 0))
-const result: number = solve('godding', 'dn', 0, 0);
+const result = solve('godding', 'dn', 0, 0);
 console.log(result)
 
-
+// dn
 //        'd'
 //      /    \
-//      4     2
+//      4     2 
 //   /   \   /  \
-//  4     2 4    3
+//  4     2 4    3  n
